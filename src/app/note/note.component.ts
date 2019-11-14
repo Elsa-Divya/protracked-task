8df97c95e711cff849ea6ebb9097b89e57ab6550
Divya Elsa <divyaelsaaagnes@gmail.com>
2019-11-14 09:21:52 +0530

    removed fonts

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Note } from '../model/note';
 import { FormControl } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap';
@@ -15,21 +15,6 @@ export class NoteComponent implements OnInit {
 
 
   showCreatePanel: boolean = false;
-  showTitleInput: boolean = false;
-  note: Note;
-  title = new FormControl('');
-  content = new FormControl('');
-  selectedLabel = new FormControl();
-
-  isEditNoteActive:boolean = false;
-
-
-  labels: String[] = ["danger","testing"];
-  selected_labels: String[] = [];
-
-  modalRef:BsModalRef;
-
-
 
   constructor(private noteService:NoteService) { }
 
@@ -46,53 +31,7 @@ export class NoteComponent implements OnInit {
     this.showCreatePanel = !this.showCreatePanel;
   }
 
-  toggleTitle(){
-    this.showTitleInput = !this.showTitleInput;
-  }
 
-  createNote(){
-  
-    let newNote:Note = <Note>{
-      title : this.title.value,
-      content : this.content.value,
-      id : Math.random(),
-      color:"white",
-      labels : this.selected_labels,
-      isHidden:false
-    }
-    this.addNote(newNote);
-    this.showCreatePanel = false;
-    this.resetNote();
-
-  }
-
-  addNote(note){
-    this.noteService.addNotes(note);
-  }
-
-  
-
-  addLabelToNote(e){
-    
-    if(e.target.checked){
-      console.log("Adding label to note id :",e.target.value);
-      this.selected_labels.push(e.target.value);
-    }else{
-     this.selected_labels = this.selected_labels.filter(label=>{
-        console.log(label,e.target.value)
-        return e.target.value !=label
-      })
-    }
-
-    console.log(this.selected_labels)
-    
-  }
-
-  resetNote(){
-    this.title.reset();
-    this.content.reset();
-    this.selected_labels = [];
-  }
 
 
 
